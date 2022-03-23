0ad9bae38bf16968f2dc8704b9ba83e6d97c1e08
Alireza Sedghi <alirezasedghi911@gmail.com>
2022-03-24 02:11:36 +0430

    update virastar documentations

--- virastar/assets/js/main.js
+++ virastar/assets/js/main.js
@@ -1,48 +1,90 @@
 (function () {
-  "use strict";
+    "use strict";
 
-  // ======= Sticky
-  window.onscroll = function () {
-    // show or hide the back-top-top button
-    const backToTop = document.querySelector(".back-to-top");
-    if (
-      document.body.scrollTop > 50 ||
-      document.documentElement.scrollTop > 50
-    ) {
-      backToTop.style.display = "flex";
-    } else {
-      backToTop.style.display = "none";
-    }
-  };
-
-  // ====== scroll top js
-  function scrollTo(element, to = 0, duration = 500) {
-    const start = element.scrollTop;
-    const change = to - start;
-    const increment = 20;
-    let currentTime = 0;
-
-    const animateScroll = () => {
-      currentTime += increment;
-
-      element.scrollTop = Math.easeInOutQuad(currentTime, start, change, duration);
-
-      if (currentTime < duration) {
-        setTimeout(animateScroll, increment);
-      }
+    // ======= Sticky
+    window.onscroll = function () {
+        // show or hide the back-top-top button
+        const backToTop = document.querySelector(".back-to-top");
+        if (
+            document.body.scrollTop > 50 ||
+            document.documentElement.scrollTop > 50
+        ) {
+            backToTop.style.display = "flex";
+        } else {
+            backToTop.style.display = "none";
+        }
     };
 
-    animateScroll();
-  }
+    // ====== scroll top js
+    function scrollTo(element, to = 0, duration = 500) {
+        const start = element.scrollTop;
+        const change = to - start;
+        const increment = 20;
+        let currentTime = 0;
 
-  Math.easeInOutQuad = function (t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return (c / 2) * t * t + b;
-    t--;
-    return (-c / 2) * (t * (t - 2) - 1) + b;
-  };
+        const animateScroll = () => {
+            currentTime += increment;
 
-  document.querySelector(".back-to-top").onclick = () => {
-    scrollTo(document.documentElement);
-  };
+            element.scrollTop = Math.easeInOutQuad(currentTime, start, change, duration);
+
+            if (currentTime < duration) {
+                setTimeout(animateScroll, increment);
+            }
+        };
+
+        animateScroll();
+    }
+
+    Math.easeInOutQuad = function (t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return (c / 2) * t * t + b;
+        t--;
+        return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    document.querySelector(".back-to-top").onclick = () => {
+        scrollTo(document.documentElement);
+    };
+
+    // ==== for menu scroll
+    const pageLink = document.querySelectorAll(".ud-menu-scroll");
+
+    pageLink.forEach((elem) => {
+        elem.addEventListener("click", (e) => {
+            e.preventDefault();
+            document.querySelector(elem.getAttribute("href")).scrollIntoView({
+                behavior: "smooth",
+                offsetTop: 1 - 60,
+            });
+        });
+    });
+
+// section menu active
+    function onScroll(event) {
+        const sections = document.querySelectorAll(".ud-menu-scroll");
+        const scrollPos =
+            window.pageYOffset ||
+            document.documentElement.scrollTop ||
+            document.body.scrollTop;
+
+        for (let i = 0; i < sections.length; i++) {
+            const currLink = sections[i];
+            const val = currLink.getAttribute("href");
+            const refElement = document.querySelector(val);
+            const scrollTopMinus = scrollPos + 73;
+            if (
+                refElement.offsetTop <= scrollTopMinus &&
+                refElement.offsetTop + refElement.offsetHeight > scrollTopMinus
+            ) {
+                document
+                    .querySelector(".ud-menu-scroll")
+                    .classList.remove("active");
+                currLink.classList.add("active");
+            } else {
+                currLink.classList.remove("active");
+            }
+        }
+    }
+
+    window.document.addEventListener("scroll", onScroll);
 })();
